5105b4af1cf182e6bcc1429fdfa1ae69d832c78e
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-12-03 13:34:33 -0600

    add images to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,4 +6,31 @@ app.controller('StarWarsController', ['$http', function ($http){
     console.log('Star Wars Controller has been loaded');
     var self = this;
 
+    self.profileArray = [
+        {
+            image: 'images/luke-skywalker.jpeg',
+            description: 'Luke description here'
+        },
+        {
+            image: 'images/luke-skywalker.jpeg',
+            description: 'Luke description here'
+        },
+        {
+            image: 'images/luke-skywalker.jpeg',
+            description: 'Luke description here'
+        },
+        {
+            image: 'images/luke-skywalker.jpeg',
+            description: 'Luke description here'
+        },
+        {
+            image: 'images/luke-skywalker.jpeg',
+            description: 'Luke description here'
+        },
+        {
+            image: 'images/luke-skywalker.jpeg',
+            description: 'Luke description here'
+        }
+    ];
+
 }]);
